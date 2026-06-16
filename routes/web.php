fd58308467da5e00bf2070ee18268357e0fb95bf
FhussainCodes <betterpart2300@gmail.com>
2026-06-16 12:58:58 +0500

    generate DynamicFormController using artisan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DynamicFormController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dynamic-form', [DynamicFormController::class, 'showForm'])->name('form.show');
+Route::post('/dynamic-form', [DynamicFormController::class, 'storeData'])->name('form.store');
